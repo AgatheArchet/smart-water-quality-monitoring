3f397412850da87a4aa9ec10ac9eaa12efbbc854
AgatheArchet <agathe.archet@ensta-bretagne.org>
2020-05-27 12:01:30 +0200

    management of data received from bluno throught BLE

--- RaspberryBleRos/ble_interface.py
+++ RaspberryBleRos/ble_interface.py
@@ -3,6 +3,8 @@ import struct
 import time
 from bluepy.btle import Peripheral, UUID, DefaultDelegate
  
+# tutorial for bluetooth settings : https://learn.adafruit.com/install-bluez-on-the-raspberry-pi/installation
+
 class SensorsDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
@@ -10,43 +12,28 @@ class SensorsDelegate(DefaultDelegate):
 
     def handleNotification(self, cHandle, data):
         if (cHandle==37):
-            msg = data.decode()
-            if(msg[0]=="M"):
-                cursor = 0
-                if(msg[cursor+2]=='d'):
-                    month = msg[cursor+1]
-                else:
-                    month = msg[cursor+1:cursor+3]
-                cursor += len(month)+1
-                if(msg[cursor+2]=='y'):
-                    day = msg[cursor+1]
-                else:
-                    day = msg[cursor+1:cursor+3]
-                cursor += len(day)+1
-                year = msg[cursor+1:cursor+5]
-                cursor += 5
-                if(msg[cursor+2]=='m'):
-                    hour = msg[cursor+1]
-                else:
-                    hour = msg[cursor+1:cursor+3]
-                cursor += len(hour)+1
-                if(msg[cursor+2]=='s'):
-                    minute = msg[cursor+1]
-                else:
-                    minute = msg[cursor+1:cursor+3]
-                cursor += len(minute)+1
-                second = msg[cursor+1:]
-                print(month+"/"+day+"/"+year+"  "+hour+":"+minute+":"+second)
-            elif(msg[0:2]=="pH"):
-                phValue = msg[2:6]
-                tpValue = msg[8:12]
-                ecValue = msg[14:18]
-                print("pH : "+phValue+" Temp : "+tpValue+"°C  Conudctivity : "+ecValue+" mS/cm")
-            elif(msg[0:2]=="Do"):
-                doValue = msg[2:6]
-                orValue = msg[8:12]
-                print ("Dissolved Oxygen : "+doValue+"  Redox potential : "+orValue)
-        
+            if (len(data)>6):
+                msg = data.decode()
+                if(msg[0]=="M"):
+                    try:
+                        month, day, year, hour, minute, second = msg.replace("M"," ").replace("d"," ").replace("y"," ").replace("h"," ").replace("m"," ").replace("s"," ").split()
+                        print(month+"/"+day+"/"+year+"  "+hour+":"+minute+":"+second)
+                    except ValueError:
+                        pass
+                elif(msg[0:2]=="pH"):
+                    try:
+                        phValue, tpValue, ecValue = msg.replace("pH"," ").replace("tp"," ").replace("EC"," ").split()
+                        print("pH : "+phValue+" Temp : "+tpValue+"°C  Conductivity : "+ecValue+" mS/cm")
+                    except ValueError:
+                        pass
+                elif(msg[0:2]=="Do"):
+                    try:
+                        doValue, orValue = msg.replace("Do"," ").replace("Or"," ").split()
+                        print ("Dissolved Oxygen : "+doValue+" mg/L  Redox potential : "+orValue+" mV")
+                    except ValueError:
+                        pass
+            else:
+                pass
 
 # connection to the device
 bluno = Peripheral("C8:DF:84:24:27:F6", "public")
