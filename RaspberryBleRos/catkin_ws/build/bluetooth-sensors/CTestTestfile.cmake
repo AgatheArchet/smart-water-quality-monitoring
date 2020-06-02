145faf680999bb15fb027dbb5fc3f52ccd3f3a50
AgatheArchet <agathe.archet@ensta-bretagne.org>
2020-06-02 16:17:31 +0200

    gitignore update

--- RaspberryBleRos/catkin_ws/build/bluetooth-sensors/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/catamaran/smart-water-quality-monitoring/RaspberryBleRos/catkin_ws/src/bluetooth-sensors
-# Build directory: /home/catamaran/smart-water-quality-monitoring/RaspberryBleRos/catkin_ws/build/bluetooth-sensors
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- RaspberryBleRos/catkin_ws/build/bluetoothSensors/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/catamaran/smart-water-quality-monitoring/RaspberryBleRos/catkin_ws/src/bluetoothSensors
-# Build directory: /home/catamaran/smart-water-quality-monitoring/RaspberryBleRos/catkin_ws/build/bluetoothSensors
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
