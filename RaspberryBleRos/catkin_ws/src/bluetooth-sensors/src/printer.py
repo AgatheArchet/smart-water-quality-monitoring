a17068b797a9adf3e0a3968fbe2809012c4a7eff
AlexandreArgento <alexandre.argento@ensta-bretagne.org>
2020-06-03 09:29:50 +0100

    attempts with rospy

--- RaspberryBleRos/catkin_ws/src/bluetooth-sensors/src/printer.py
+++ RaspberryBleRos/catkin_ws/src/bluetooth-sensors/src/printer.py
@@ -7,7 +7,15 @@ Created on Tue Jun  2 14:48:40 2020
 """
 
 import rospy
-
+from std_msgs.msg import String 
 rospy.init_node('printer_node')
-print ("Hello world")
+pub = rospy.Publisher('/phrases', String, queue_size=18)
+
+rate = rospy.Rate(10)
+msg_str = String()
+msg_str = "Hellow world"
+
+while not rospy.is_shutdown():
+    pub.publish(msg_str)
+    rate.sleep()
     
